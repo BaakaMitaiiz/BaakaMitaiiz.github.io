268875f01996762e2b0e643c77b9037793079533
BaakaMitaiiz <98559800+BaakaMitaiiz@users.noreply.github.com>
2023-04-01 20:30:12 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ const observer = new IntersectionObserver((entries) => {
 	
 });
 
-
-
-
-
-
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
